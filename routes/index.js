b4a4176d09af5e016ce320d21d681535674b49ee
Gradey Cullins <gradeycullins@gmail.com>
2015-08-17 23:54:47 -0700

    added bootstrap template, next step is to use npm package to allow users to enter steam profile url and output the steam64 id

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+var SteamID = require('steamid');
+
+// my actual steam id
+var sid = new SteamID('76561198062707855');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Steam-friends-manager' });
+  res.render('index', { 
+  	title: 'Steam Friends Manager',
+  	stuff: sid });
 });
 
 module.exports = router;
